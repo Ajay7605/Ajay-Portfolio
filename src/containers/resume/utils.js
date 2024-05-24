13e503b22aa517fa6bc439e9451526a4bad848a1
Ajay7605 <ajaykumarshetty539@gmail.com>
2024-05-24 09:01:47 +0530

    first commit

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,35 @@
+export const data = {
+    experience: [
+      {
+        title: "Intern",
+        subTitle: "At Prodigy Info-Tech",
+        description:
+          "Web Development",
+      },
+      
+    ],
+    education: [
+      {
+        title: "Secondary:",
+        subTitle: "Indo-English High School",
+        description:
+          "10th Grade : 9.7",
+        date:"2019"
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "Sri Chaitanya Junior College",
+        description:
+          "12th Grade: 976",
+          date:"2021"
+      },
+      {
+        title: "B.Tech",
+        subTitle: "Parul University",
+        description:
+          "Bachelor of Engineering technology in Computer Science",
+        description:"grade:7.95",
+        date:"2021 - Present"
+      },
+    ],
+  };
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,55 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "70",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "70",
+        },
+        {
+          skillName: "React.JS",
+          percentage: "70",
+        },
+      ],
+    },
+    {
+      label: "BACK END",
+      data: [
+        
+        {
+          skillName: "PYTHON",
+          percentage: "70",
+        },
+        {
+          skillName: "Node.JS",
+          percentage: "35",
+        },
+        {
+          skillName: "JAVA",
+          percentage: "50",
+        },
+      ],
+    },
+    {
+      label: "DATABASES",
+      data: [
+        {
+          skillName: "SQL",
+          percentage: "70",
+        },
+        {
+          skillName: "MONGO DB",
+          percentage: "65",
+        },
+        
+      ],
+    },
+  ];
